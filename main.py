51811454b072280bb69f3ce7e77e408954166107
JunCS <fara.300300@gmail.com>
2024-06-17 22:31:30 +0700

    StartWindow

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main(page: ft.Page):
         width=200,
         height=200,
         fit=ft.ImageFit.CONTAIN,
-        animate_scale= ft.Animation(duration=300,curve=ft.AnimationCurve.EASE)
+        animate_scale= ft.animation.Animation(duration=300,curve=ft.AnimationCurve.EASE)
     )
    
     txt_number = ft.TextField(
@@ -21,7 +21,7 @@ def main(page: ft.Page):
         txt_number.value = str(int(txt_number.value) + 1)
         img.sacle = 0.95
         page.update()
-        time.sleep(1)
+        time.sleep(12)
         img.sacle = 1
         page.update()
 
