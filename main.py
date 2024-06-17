d387c97cddd94eafb64e3ce7f1e75bd389927fbe
JunCS <fara.300300@gmail.com>
2024-06-17 22:29:51 +0700

    StartWindow

--- main.py
+++ main.py
@@ -1,31 +1,45 @@
 import flet as ft
+import time
 
 
 def main(page: ft.Page):
-    page.title = "Flet counter example"
+    page.title = "BrawlGems"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
-
+    
+    img = ft.Image(
+        src=f"D:\Yandex\BrawlGems\images\heroes\default\shelly\shelly_default.png",
+        width=200,
+        height=200,
+        fit=ft.ImageFit.CONTAIN,
+        animate_scale= ft.Animation(duration=300,curve=ft.AnimationCurve.EASE)
+    )
+   
     txt_number = ft.TextField(
         value="0", text_align=ft.TextAlign.RIGHT, width=100)
 
-    def minus_click(e):
-        txt_number.value = str(int(txt_number.value) - 1)
-        page.update()
-
     def plus_click(e):
         txt_number.value = str(int(txt_number.value) + 1)
+        img.sacle = 0.95
+        page.update()
+        time.sleep(1)
+        img.sacle = 1
         page.update()
 
     page.add(
         ft.Row(
             [
-                ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
-                txt_number,
-                ft.IconButton(ft.icons.ADD, on_click=plus_click),
+               ft.IconButton(content = img, on_click=plus_click,)
             ],
             alignment=ft.MainAxisAlignment.CENTER,
-        )
-    )
+        ),
+        ft.Row(spacing=0,controls=
+            [
+                txt_number
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+        ),
+      )
+
 
 
 ft.app(main)
